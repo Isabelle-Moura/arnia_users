7af76649fddeb90293eab7f068a552fdda28077f
Isabelle-Moura <mourabisabelle@gmail.com>
2024-04-16 21:31:19 -0300

    feat: added e2e tests for users.

--- src/users/users.e2e.spec.ts
+++ src/users/users.e2e.spec.ts
@@ -1,14 +1,14 @@
-import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthGuard } from '../auth/guards/auth.guard';
 import { authGuard } from '../testing/guards/auth-guard.mock';
 import { userRepositoryMock } from '../testing/users/users-repository.mock';
 import { UsersModule } from './users.module';
-
-/* 
-  [] TODO: Continuar o Swagger.
-  [] TODO: Testar todas as rotas.
-*/
+import { updateUserDtoMock } from '../testing/users/update-user-dto.mock';
+import { userMock } from '../testing/users/user.mock';
+import { usersMock } from '../testing/users/users.mock';
+import { createUserMockDto } from '../testing/users/create-user-dto.mock';
+import * as request from 'supertest';
 
 describe('Users (e2e)', () => {
   let app: INestApplication;
@@ -36,10 +36,60 @@ describe('Users (e2e)', () => {
     expect(app).toBeDefined();
   });
 
-  it('should create an user', () => {});
-  it('should return all users', () => {});
-  it('should return one user', () => {});
-  it('should update an user', () => {});
-  it('should delete an user', () => {});
-  it('should retunr an logged user', () => {});
+  describe('create', () => {
+    it('/users (POST)', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/users')
+        .send(createUserMockDto);
+
+      expect(response.statusCode).toEqual(HttpStatus.CREATED);
+      expect(response.body).toEqual(userMock);
+    });
+  });
+
+  describe('findAll', () => {
+    it('/users (GET)', async () => {
+      const response = await request(app.getHttpServer()).get('/users');
+
+      expect(response.statusCode).toEqual(HttpStatus.OK);
+      expect(response.body).toEqual(usersMock);
+    });
+  });
+
+  describe('findOne', () => {
+    it('/users/:id (GET)', async () => {
+      const response = await request(app.getHttpServer()).get('/users/1');
+
+      expect(response.statusCode).toEqual(HttpStatus.OK);
+      expect(response.body).toEqual(userMock);
+    });
+  });
+
+  describe('update', () => {
+    it('/users/:id (PATCH)', async () => {
+      const response = await request(app.getHttpServer())
+        .patch('/users/1')
+        .send(updateUserDtoMock);
+
+      expect(response.statusCode).toEqual(HttpStatus.OK);
+      expect(response.body).toEqual(userMock);
+    });
+  });
+
+  describe('delete', () => {
+    it('/users/:id (DELETE)', async () => {
+      const response = await request(app.getHttpServer()).delete('/users/1');
+
+      expect(response.statusCode).toEqual(HttpStatus.NO_CONTENT);
+    });
+  });
+
+  describe('profile', () => {
+    it('/users/profile (GET)', async () => {
+      const response = await request(app.getHttpServer()).get('/users/profile');
+
+      expect(response.statusCode).toEqual(HttpStatus.OK);
+      expect(response.body).toEqual(userMock);
+    });
+  });
 });
